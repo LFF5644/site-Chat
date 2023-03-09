725398d6ceffb886999dd15b13076f9f2e9f1d08
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-03-09 17:59:50 +0100

    STYLE UPDATE!!!

--- app.js
+++ app.js
@@ -1,29 +1,81 @@
 const socket=io({path:"/bind/socket/Chat"});
 const token=unescape('<?=ESC(input.token)?>');
+const history=[];
 let account=null;
 
 const ids={
 	h1_chat: document.getElementById("h1_chat"),
-	textarea_chatHistory: document.getElementById("textarea_chatHistory"),
 	button_send: document.getElementById("button_send"),
 	input_msg: document.getElementById("input_msg"),
 	form_msg: document.getElementById("form_msg"),
+	div_chat: document.getElementById("div_chat"),
 };
 
-function writeHistory(text){
-	textarea_chatHistory.value+=text;
+function getTime(time=Date.now()){
+	const date=new Date(time);
+	let str=String(date.getHours()).padStart(2,"0");
+	str+=":";
+	str+=String(date.getMinutes()).padStart(2,"0");
+	return str;
+}
+function appendHistory(data){
+	let {type,text,user,time}=data;
+	history.push(data);
+	
+	if(typeof(data)=="string") text=data;
+	if(text) text=text.trim();
+	if(!time) time=Date.now();
+	
+
+	const div=document.createElement("div");
+	div.innerHTML="";
+	if(type=="msg"){
+		const me=user.username==account.username;
+		div.classList.add("msg");
+		if(me) div.classList.add("me");
+
+		const h3_nickname=document.createElement("p");
+		h3_nickname.classList.add("nickname");
+		h3_nickname.innerText=user.nickname;
+		h3_nickname.title=user.username;
+		div.appendChild(h3_nickname);
+
+		const p_text=document.createElement("p");
+		p_text.classList.add("text");
+		p_text.innerText=text;
+		div.appendChild(p_text);
+
+		const p_time=document.createElement("p");
+		p_time.classList.add("time");
+		p_time.innerText=getTime(time);
+		div.appendChild(p_time);
+	}
+	else if(type=="info"||typeof(data)=="string"){
+		div.classList.add("info");
+
+		const p_text=document.createElement("p");
+		p_text.classList.add("text");
+		p_text.innerText=text;
+		div.appendChild(p_text);
+
+		const p_time=document.createElement("p");
+		p_time.classList.add("time");
+		p_time.innerText=getTime(time);
+		div.appendChild(p_time);
+	}
+	ids.div_chat.appendChild(div);
 }
 function sendMsg(){
 	const msg=ids.input_msg.value;
 	socket.emit("send-msg",{msg});
-	writeHistory(account.nickname+": "+msg+"\n");
+	appendHistory(account.nickname+": "+msg+"\n");
 }
 
 socket.on("connect",()=>{
 	console.log("connected as "+socket.id);
 	ids.h1_chat.style.color="unset";
 	ids.h1_chat.title="Verbindung zum Server hergestellt!";
-	writeHistory("Verbindung Hergestellt!\n");
+	appendHistory("Verbindung Hergestellt!\n");
 });
 socket.on("get-token",()=>{
 	socket.emit("get-token",token);
@@ -31,30 +83,30 @@ socket.on("get-token",()=>{
 socket.on("account-err",msg=>{
 	alert("Sie sind NICHT angemeldet der chat kann nur mit einem Account verwendet werden. Klicken Sie jz auf Ok um sich anzumelden!");
 	location.href="/account?goto=Chat";
-	writeHistory("ACCOUNT ERROR!\n");
+	appendHistory("ACCOUNT ERROR!\n");
 });
 socket.on("receive-msg",data=>{
 	const {user,time,msg}=data;
 	console.log(user.nickname+": "+msg);
-	writeHistory(user.nickname+": "+msg+"\n");
+	appendHistory(user.nickname+": "+msg+"\n");
 });
 socket.on("get-myUser",accountData=>{
 	account=accountData;
-	writeHistory(`Angemeldet als: ${account.nickname} (${account.username})\n`);
+	appendHistory(`Angemeldet als: ${account.nickname} (${account.username})\n`);
 });
 socket.on("user-disconnect",data=>{
 	const {user}=data;
-	writeHistory(user.nickname+" hat die Verbindung getrennt\n");
+	appendHistory(user.nickname+" hat die Verbindung getrennt\n");
 });
 socket.on("user-connect",data=>{
 	const {user}=data;
-	writeHistory(user.nickname+" hat sich Verbunden\n");
+	appendHistory(user.nickname+" hat sich Verbunden\n");
 });
 socket.on("disconnect",()=>{
 	console.log("disconnected");
 	ids.h1_chat.style.color="red";
 	ids.h1_chat.title="Keine Verbindung zum Server!";
-	writeHistory("Verbindung zum Server getrennt\n");
+	appendHistory("Verbindung zum Server getrennt\n");
 });
 
 ids.form_msg.onsubmit=event=>{
