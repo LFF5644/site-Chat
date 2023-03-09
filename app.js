68e736947ff203fea44bbcb5a05cb70fd45e4a85
LFF5644 (Code-Server) <meerschweinchen5644@gmail.com>
2023-03-09 09:14:12 +0100

    init; working

---
+++ app.js
@@ -0,0 +1,57 @@
+const socket=io({path:"/bind/socket/Chat"});
+const token=unescape('<?=ESC(input.token)?>');
+let account=null;
+
+const ids={
+	h1_chat: document.getElementById("h1_chat"),
+	textarea_chatHistory: document.getElementById("textarea_chatHistory"),
+	button_send: document.getElementById("button_send"),
+	input_msg: document.getElementById("input_msg"),
+	form_msg: document.getElementById("form_msg"),
+};
+
+function writeHistory(text){
+	textarea_chatHistory.value+=text;
+}
+function sendMsg(){
+	const msg=ids.input_msg.value;
+	socket.emit("send-msg",{msg});
+	writeHistory(account.nickname+": "+msg+"\n");
+}
+
+socket.on("connect",()=>{
+	console.log("connected as "+socket.id);
+	ids.h1_chat.style.color="unset";
+	ids.h1_chat.title="Verbindung zum Server hergestellt!";
+	writeHistory("Verbindung Hergestellt!\n");
+});
+socket.on("get-token",()=>{
+	socket.emit("get-token",token);
+});
+socket.on("account-err",msg=>{
+	alert("Sie sind NICHT angemeldet der chat kann nur mit einem Account verwendet werden. Klicken Sie jz auf Ok um sich anzumelden!");
+	location.href="/account?goto=Chat";
+	writeHistory("ACCOUNT ERROR!\n");
+});
+socket.on("receive-msg",data=>{
+	const {user,time,msg}=data;
+	console.log(user.nickname+": "+msg);
+	writeHistory(user.nickname+": "+msg+"\n");
+});
+socket.on("get-myUser",accountData=>{
+	account=accountData;
+	writeHistory(`Angemeldet als: ${account.nickname} (${account.username})\n`);
+})
+socket.on("disconnect",()=>{
+	console.log("disconnected");
+	ids.h1_chat.style.color="red";
+	ids.h1_chat.title="Keine Verbindung zum Server!";
+	writeHistory("Verbindung zum Server getrennt\n");
+});
+
+ids.form_msg.onsubmit=event=>{
+	event.preventDefault();
+	sendMsg();
+	ids.input_msg.value="";
+	ids.input_msg.focus();
+};
---
+++ chat.service.js
@@ -0,0 +1,60 @@
+const socketIo=require("socket.io");
+const services={
+	account: service_require("server/account/account.new"),
+}
+
+this.start=()=>{
+	this.clients={};
+
+	this.io=socketIo(23863,{
+		cors:{
+			origin:"*",
+		},
+	});
+	this.io.on("connect",socket=>{
+		this.clients[socket.id]={
+			token: null,
+			account: null,
+			accountIndex: null,
+		};
+		socket.emit("get-token");
+		socket.on("get-token",token=>{
+			this.clients[socket.id].token=token;
+			const login=services.account.authUserByInput({
+				token,
+			});
+			if(!login.allowed){
+				socket.emit("account-err","wrong-token");
+				socket.disconnect();
+				return;
+			}
+			this.clients[socket.id].account=login.data.account;
+			this.clients[socket.id].accountIndex=login.data.accountIndex;
+			const client=this.clients[socket.id];
+			socket.emit("get-myUser",{
+				username: client.account.username,
+				nickname: client.account.nickname,
+			});
+		});
+		socket.on("send-msg",data=>{
+			const {msg}=data;
+			const client=this.clients[socket.id];
+			if(!client.token) return;
+			socket.broadcast.emit("receive-msg",{
+				user:{
+					username: client.account.username,
+					nickname: client.account.nickname,
+				},
+				time: Date.now(),
+				msg,
+			});
+			socket.emit("msg-sended");
+		});
+		socket.on("disconnect",()=>{
+			this.clients[socket.id]=undefined;
+		});
+	});
+}
+this.stop=()=>{
+
+}
